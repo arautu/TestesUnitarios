638ea071937cf3caa6d93a6c7db74785d5b71946
Leandro Oliveira <leandrolive@gmail.com>
2020-05-07 10:36:38 -0300

    Testando sem ferramentas
    Correção de erro de compilação, conforme ticket #1.

---
+++ src/main/java/br/ce/wcaquino/services/LocacaoService.java
@@ -0,0 +1,47 @@
+package br.ce.wcaquino.services;
+
+import br.ce.wcaquino.entities.Filme;
+import br.ce.wcaquino.entities.Locacao;
+import br.ce.wcaquino.entities.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+
+import java.util.Date;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
+
+public class LocacaoService {
+	
+	public Locacao alugarFilme(Usuario usuario, Filme filme) {
+		Locacao locacao = new Locacao();
+		locacao.setFilme(filme);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(filme.getPrecoLocacao());
+
+		//Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		//Salvando a locacao...
+		//TODO adicionar método para salvar
+
+		return locacao;
+	}
+
+	public static void main(String[] args) {
+		// cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 2, 4.0);
+
+		// ação
+		Locacao locacao = service.alugarFilme(usuario, filme);
+
+		// verificação
+		System.out.println(locacao.getValor() == 4.0);
+		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(),
+				DataUtils.obterDataComDiferencaDias(1)));
+	}
+}
