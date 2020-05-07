2634a49f9a38937a0e298fb04e112bdac4426fc0
Leandro Oliveira <leandrolive@gmail.com>
2020-05-07 16:25:35 -0300

    Formas de dividir um teste 1

--- src/test/java/br/ce/wcaquino/services/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/services/LocacaoServiceTest.java
@@ -13,20 +13,28 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LocacaoServiceTest {
+
+    // cenario
+    LocacaoService service = new LocacaoService();
+    Usuario usuario = new Usuario("Usuario 1");
+    Filme filme = new Filme("Filme 1", 2, 5.0);
+
+    // ação
+    Locacao locacao = service.alugarFilme(usuario, filme);
+
     @Test
-    public void teste() {
-        // cenario
-        LocacaoService service = new LocacaoService();
-        Usuario usuario = new Usuario("Usuario 1");
-        Filme filme = new Filme("Filme 1", 2, 5.0);
-
-        // ação
-        Locacao locacao = service.alugarFilme(usuario, filme);
-
+    public void testeLocacaoValor() {
         // verificação
         assertThat(locacao.getValor(), is(equalTo(5.0)));
-        assertThat(locacao.getValor(), is(not(6.0)));
+    }
+
+    @Test
+    public void testeDataLocacao() {
         assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+    }
+
+    @Test
+    public void testeDataRetorno() {
         assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)),
                 is(true));
     }
